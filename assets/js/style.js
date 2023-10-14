a41e741807d03e925db8d4c1d8a7f95df378c6f5
riley.lin <rileylin.space@gmail.com>
2023-10-14 12:42:27 +0800

    add new

--- assets/js/style.js
+++ assets/js/style.js
@@ -34,3 +34,20 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+// NEW SWIPER
+let swiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true
+    },
+    breakpoints: {
+        992: {
+            spaceBetween: 80
+        }
+    }
+})

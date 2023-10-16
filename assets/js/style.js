ede61150028612184e56ae07a0b722a1168c0de5
riley.lin <rileylin.space@gmail.com>
2023-10-16 11:02:04 +0800

    add scroll section active link

--- assets/js/style.js
+++ assets/js/style.js
@@ -51,3 +51,24 @@ let swiper = new Swiper(".new-swiper", {
         }
     }
 })
+
+// SCROLL SECTION ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)

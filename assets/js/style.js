88aaf060c21894627856ccc8af88177c35ecf597
riley.lin <rileylin.space@gmail.com>
2023-10-16 11:11:06 +0800

    add scroll up

--- assets/js/style.js
+++ assets/js/style.js
@@ -72,3 +72,13 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll', scrollActive)
+
+// SHOW SCROLL UP  
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+
+    if (this.scrollY >= 50) scrollUp.classList.add('show-scroll')
+    else scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)

bb068e38ba3629d122d0a3392403a469d7d1fe46
riley.lin <rileylin.space@gmail.com>
2023-10-16 11:27:02 +0800

    add dark & light theme

--- assets/js/style.js
+++ assets/js/style.js
@@ -82,3 +82,27 @@ function scrollUp() {
 }
 
 window.addEventListener('scroll', scrollUp)
+
+// DARK LIGHT THEME
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme') ? 'dark' : 'light'
+const selectedIcon = localStorage.getItem('selected-icon') ? 'bx bx-moon' : 'bx bx-sun'
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme)
+const getCurrentIcon = () => document.body.classList.contains(iconTheme)
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove`'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
